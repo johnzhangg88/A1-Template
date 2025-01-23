ede80719136d2d8ebba46b7ff73ae9a4cf6df64f
johnzhangg88 <johntoronto2005@gmail.com>
2025-01-23 13:08:46 -0500

    Create PathFinder.java

---
+++ PathFinder.java
@@ -0,0 +1,12 @@
+public class PathFinder {
+    private final Maze maze;
+
+    public PathFinder(Maze maze) {
+        this.maze = maze;
+    }
+
+    public String findPath() {
+        // Stub implementation
+        return "F L F F R"; // Example path
+    }
+}
